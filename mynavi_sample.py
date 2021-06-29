457b829c27f29cbee3843d0c644eb3e070041034
tk <takashi002013@gmail.com>
2021-06-30 08:40:02 +0900

    1

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -1,10 +1,8 @@
 import os
-from selenium.webdriver import Chrome, ChromeOptions
 import time
 import pandas as pd
 import datetime
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver import Chrome, ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 
 LOG_FILE_PATH = "./log/log_{datetime}.log"
@@ -26,25 +24,27 @@ def set_driver(driver_path, headless_flg):
     # options.add_argument('log-level=3')
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
-    options.add_argument('--incognito')          # シークレットモードの設定を付与
+    options.add_argument('--incognito') # シークレットモードの設定を付与
 
     # ChromeのWebDriverオブジェクトを作成する。
     return Chrome(ChromeDriverManager().install(), options=options)
 
 ### ログファイルおよびコンソール出力
 def log(txt):
-    now=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     logStr = '[%s: %s] %s' % ('log',now , txt)
     # ログ出力
     with open(log_file_path, 'a', encoding='utf-8_sig') as f:
         f.write(logStr + '\n')
     print(logStr)
 
+
 def find_table_target_word(th_elms, td_elms, target:str):
     # tableのthからtargetの文字列を探し一致する行のtdを返す
     for th_elm,td_elm in zip(th_elms,td_elms):
         if th_elm.text == target:
             return td_elm.text
+
 
 ### main処理
 def main():
@@ -69,15 +69,12 @@ def main():
     driver.find_element_by_class_name("topSearch__text").send_keys(search_keyword)
     # 検索ボタンクリック
     driver.find_element_by_class_name("topSearch__button").click()
-
+    
     # ページ終了まで繰り返し取得
-    exp_name_list = []
-    exp_copy_list = []
-    exp_status_list = []
-    exp_first_year_fee_list = []
     count = 0
     success = 0
     fail = 0
+    df = pd.DataFrame()
     while True:
         # 検索結果の一番上の会社名を取得(まれに１行目が広告の場合、おかしな動作をするためcassetteRecruit__headingで広告を除外している)
         name_list = driver.find_elements_by_css_selector(".cassetteRecruit__heading .cassetteRecruit__name")
@@ -85,16 +82,26 @@ def main():
         status_list = driver.find_elements_by_css_selector(".cassetteRecruit__heading .labelEmploymentStatus")
         table_list = driver.find_elements_by_css_selector(".cassetteRecruit .tableCondition") # 初年度年収
         
+        '''
+        ## 様々なCSSセレクター
+        driver.find_elements_by_css_selector(".classname tagname") #クラス配下のタグ
+        driver.find_elements_by_css_selector(".classname>tagname") #クラス直下のタグ
+        driver.find_elements_by_css_selector("[name='test']") #name属性=test
+        driver.find_elements_by_css_selector("[data-test='test-data']") #data-test属性=test-data
+        '''
+        
         # 1ページ分繰り返し
         for name, copy, status, table in zip(name_list, copy_list, status_list,table_list):
             try:
                 # try~exceptはエラーの可能性が高い箇所に配置
-                exp_name_list.append(name.text)
-                exp_copy_list.append(copy.text)
-                exp_status_list.append(status.text)
                 # 初年度年収をtableから探す
                 first_year_fee = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "初年度年収")
-                exp_first_year_fee_list.append(first_year_fee)
+                # DataFrameにレコードを追加
+                df = df.append({"企業名": name.text,
+                                "キャッチコピー": copy.text,
+                                "ステータス": status.text,
+                                "初年度年収": first_year_fee},
+                                ignore_index=True)
                 log(f"{count}件目成功 : {name.text}")
                 success+=1
             except Exception as e:
@@ -114,16 +121,31 @@ def main():
             log("最終ページです。終了します。")
             break
 
-    # CSV出力
+    # 現在時刻を指定した文字列フォーマットとして取得（非常によく使う）
     now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    df = pd.DataFrame({"企業名":exp_name_list,
-                       "キャッチコピー":exp_copy_list,
-                       "ステータス":exp_status_list,
-                       "初年度年収":exp_first_year_fee_list})
-    df.to_csv(EXP_CSV_PATH.format(search_keyword=search_keyword,datetime=
-                                  now), encoding="utf-8-sig")
+    '''
+    pd.DataFrame(辞書形式のデータ)
+    辞書は以下の形式
+    {
+        "カラム名１":データのlist,
+        "カラム名２":データのlist,
+    }
+    辞書　→　名前でアクセス可能　dic["企業名"]
+    list　→　index番号でアクセス可能 li[1]
+    
+    通常はlistと辞書は組み合わせて使う
+    '''
+    # csv出力
+    df.to_csv(EXP_CSV_PATH.format(search_keyword=search_keyword, datetime=now), encoding="utf-8-sig")
+    
+    # ログの役割は、開発時や後からプログラムの動作をチェックするために用いる
+    # ログがないと、お客から何か動作がおかしいと指摘されても、確認するすべがない！
     log(f"処理完了 成功件数: {success} 件 / 失敗件数: {fail} 件")
     
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
 if __name__ == "__main__":
     main()
+    
+    
+
+
