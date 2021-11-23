67c40b3c6ab177bea5c9e80e9b08762d5f451219
takashi002013@gmail.com <takashi002013@gmail.com>
2021-11-23 10:00:26 +0900

    Selenium version4に対応

--- scraping.py
+++ scraping.py
@@ -1,10 +1,15 @@
 import os
 import time
-import pandas as pd
 import datetime
+
 from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import fire
+import pandas as pd
+
+# Selenium version4対応済
 
 # 定数は大文字の変数名が一般的
 # {}に変数名をセットすると、後でformatで置換可能
@@ -13,31 +18,28 @@ EXP_CSV_PATH="results/exp_list_{search_keyword}_{datetime}.csv"
 log_file_path=LOG_FILE_PATH.format(datetime=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
 
 
-def set_driver(driver_path, headless_flg):
+def set_driver(hidden_chrome: bool=False):
     '''
-    Selenium用のChromeを起動する
+    Chromeを自動操作するためのChromeDriverを起動してobjectを取得する
     '''
-    # Chromeドライバーの読み込み
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
     options = ChromeOptions()
 
     # ヘッドレスモード（画面非表示モード）をの設定
-    if headless_flg == True:
+    if hidden_chrome:
         options.add_argument('--headless')
 
     # 起動オプションの設定
-    # User-agentはブラウザと同様のものを使用する。指定しないとサイトによってはブロックされる可能性がある
-    options.add_argument(
-        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
-    
-    # 不要なエラーを非表示にする
-    options.add_argument('log-level=3')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
+    options.add_argument(f'--user-agent={USER_AGENT}') # ブラウザの種類を特定するための文字列
+    options.add_argument('log-level=3') # 不要なログを非表示にする
+    options.add_argument('--ignore-certificate-errors') # 不要なログを非表示にする
+    options.add_argument('--ignore-ssl-errors') # 不要なログを非表示にする
+    options.add_experimental_option('excludeSwitches', ['enable-logging']) # 不要なログを非表示にする
     options.add_argument('--incognito') # シークレットモードの設定を付与
-
-    # ChromeのWebDriverオブジェクトを作成する。
-    # ChromeDriverManager().install() はChromeDriverの最新版をdownloadして、そのPATHを返す
-    return Chrome(ChromeDriverManager().install(), options=options)
+    
+    # ChromeのWebDriverオブジェクトを作成する。(Selenium version4からは以下のようにServiceを使用することが推奨される)
+    service=Service(ChromeDriverManager().install())
+    return Chrome(service=service, options=options)
 
 
 def makedir_for_filepath(filepath: str):
@@ -62,9 +64,9 @@ def log(txt):
     print(logStr)
 
 
-def find_table_target_word(th_elms, td_elms, target:str):
+def find_table_col_by_header_name(th_elms, td_elms, target:str):
     '''
-    table要素の中から、targetで指定したheaderを探し、対応するdataを取得する
+    table要素の中から、targetで指定したheaderを探し、対応するカラムのdataを取得する
     '''
     # tableのthからtargetの文字列を探し一致する行のtdを返す
     for th_elm,td_elm in zip(th_elms,td_elms):
@@ -72,17 +74,17 @@ def find_table_target_word(th_elms, td_elms, target:str):
             return td_elm.text
 
 
-def main(is_option: bool = False, page_limit: int=5):
+def main(is_option: bool = False, page_limit: int=5, hidden_chrome: bool=False):
     '''
     main処理
      - is_option: Trueでオプション課題用のurlの直接指定機能を動作させる
      - page_limit: スクレピングするページ数の上限を指定
     '''
-    log(f"処理開始: is_option={is_option}, page_limit={page_limit}")
+    log(f"処理開始: is_option={is_option}, page_limit={page_limit}, hidden_chrome={hidden_chrome}")
     search_keyword=input("検索キーワードを入力してください：")
     log("検索キーワード:{}".format(search_keyword))
     # driverを起動
-    driver = set_driver("chromedriver.exe", False)
+    driver = set_driver(hidden_chrome)
     
     # Webサイトを開く
     if is_option:
@@ -102,9 +104,10 @@ def main(is_option: bool = False, page_limit: int=5):
             pass
 
         # 検索窓に入力
-        driver.find_element_by_class_name("topSearch__text").send_keys(search_keyword)
+        # Selenium version4からfind_element_by_*のメソッドは非推奨となったため、以下の通りfind_elementを使用
+        driver.find_element(by=By.CLASS_NAME, value="topSearch__text").send_keys(search_keyword)
         # 検索ボタンクリック
-        driver.find_element_by_class_name("topSearch__button").click()
+        driver.find_element(by=By.CLASS_NAME, value="topSearch__button").click()
 
     # ページ終了まで繰り返し取得
     count = 0
@@ -114,26 +117,26 @@ def main(is_option: bool = False, page_limit: int=5):
     page = 1
     while page <= page_limit:
         # 求人の要素を丸ごと取得
-        recruit_elms = driver.find_elements_by_css_selector(".cassetteRecruit")
-        
+        recruit_elms = driver.find_elements(by=By.CSS_SELECTOR, value=".cassetteRecruit")
         '''
-        ## 様々なCSSセレクター
-        driver.find_elements_by_css_selector(".classname tagname") #クラス配下のタグ
-        driver.find_elements_by_css_selector(".classname>tagname") #クラス直下のタグ
-        driver.find_elements_by_css_selector("[name='test']") #name属性=test
-        driver.find_elements_by_css_selector("[data-test='test-data']") #data-test属性=test-data
+        ## 色々な指定方法
+        by=By.CSS_SELECTOR のようにして指定方法を選択
+        By.ID By.NAME By.CLASS_NAME By.LINK_TEXT などがあるので
+        Byと入力して、VSCODEの予測変換で確認してみると良いと思います。
+        概ねCSS_SELECTORで網羅できるので基本はCSS_SELECTORを推奨ですが
+        NAMEはSELECTORで指定すると面倒なので、NAMEを使った方が楽です。
         '''
         
         # 1ページ分繰り返し
         for recruit_elm in recruit_elms:
+            # try~exceptはエラーの可能性が高い箇所に配置
             try:
-                name = recruit_elm.find_element_by_css_selector(".cassetteRecruit__name").text
-                copy = recruit_elm.find_element_by_css_selector(".cassetteRecruit__copy").text
-                employment_status = recruit_elm.find_element_by_css_selector(".labelEmploymentStatus").text
-                table_elm = recruit_elm.find_element_by_css_selector("table")
-                # try~exceptはエラーの可能性が高い箇所に配置
+                name = recruit_elm.find_element(by=By.CSS_SELECTOR, value=".cassetteRecruit__name").text
+                copy = recruit_elm.find_element(by=By.CSS_SELECTOR, value=".cassetteRecruit__copy").text
+                employment_status = recruit_elm.find_element(by=By.CSS_SELECTOR, value=".labelEmploymentStatus").text
+                table_elm = recruit_elm.find_element(by=By.CSS_SELECTOR, value="table")
                 # 初年度年収をtableから探す
-                first_year_fee = find_table_target_word(table_elm.find_elements_by_tag_name("th"), table_elm.find_elements_by_tag_name("td"), "初年度年収")
+                first_year_fee = find_table_col_by_header_name(table_elm.find_elements(by=By.TAG_NAME, value="th"), table_elm.find_elements(by=By.TAG_NAME, value="td"), "初年度年収")
                 # DataFrameにレコードを追加(辞書形式でセット)
                 df = df.append({"企業名": name,
                                 "キャッチコピー": copy,
@@ -152,7 +155,7 @@ def main(is_option: bool = False, page_limit: int=5):
 
 
         # 次のページボタンがあればリンクを取得して画面遷移、なければ終了
-        next_page = driver.find_elements_by_class_name("iconFont--arrowLeft")
+        next_page = driver.find_elements(by=By.CLASS_NAME, value="iconFont--arrowLeft")
         if len(next_page) >= 1:
             next_page_link = next_page[0].get_attribute("href")
             driver.get(next_page_link)
@@ -171,13 +174,17 @@ def main(is_option: bool = False, page_limit: int=5):
     # ログの役割は、開発時や後からプログラムの動作をチェックするために用いる
     # ログがないと、お客から何か動作がおかしいと指摘されても、確認するすべがない。
     log(f"処理完了 成功件数: {success} 件 / 失敗件数: {fail} 件")
-    
+
     
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
 if __name__ == "__main__":
-    # 課題の範囲ではないが、引数を簡単に処理できるので便利
-    # 以下ように指定できる（例：is_option=True, page_limit=3 と指定される）
-    # python scraping.py 1 3
+    '''
+    課題の範囲ではないが、引数を簡単に処理できるので便利
+    - 以下ように指定できる（例：is_option=True, page_limit=3 と指定される）
+     python scraping.py 1 3
+    - 一部の引数のみの指定も可能(chromeを非表示) ハイフン２個と変数名で指定する
+     python scraping.py --hidden-chrome
+    '''
     fire.Fire(main)
               
     
